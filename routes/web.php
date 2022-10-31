10de0fa4d391d7bc4391b99a00f9289563c45f5d
Tahmeed Mahbub <18-38743-3@student.aiub.edu>
2022-11-01 00:34:41 +0600

    Minor Bug Fix

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/create', [App\Http\Controllers\HomeController::class, 'create'])->name('create');
 
